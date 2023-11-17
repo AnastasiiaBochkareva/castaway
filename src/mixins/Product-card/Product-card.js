919d16ae3b78db9499c51a349225e51b816dc50e
AnastasiiaBochkareva <120329458+AnastasiiaBochkareva@users.noreply.github.com>
2023-11-17 14:30:20 +0400

    add open-close btn

--- src/mixins/Product-card/Product-card.js
+++ src/mixins/Product-card/Product-card.js
@@ -1,8 +1,8 @@
 function showDetails() {
   document.addEventListener('click', (event) => {
     const { target } = event;
-    if (target.closest('.product-card__btn')) {
-      const parent = target.closest('.product-card__info');
+    if (target.closest('.btn__show')) {
+      const parent = target.closest('.product-card__col');
       if (parent.classList.contains('active')) {
         parent.classList.remove('active');
       } else {
